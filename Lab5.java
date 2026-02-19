e90d573bfa62697b5c2efe77d320dab37fb2392b
elperez1725 <elperez750@gmail.com>
2026-02-19 13:19:06 -0800

    Added more comments

--- Lab5.java
+++ Lab5.java
@@ -10,44 +10,73 @@ public class Lab5
     private static void problem(byte[][] arr)
     {   
        //Implement me!
+
+        // Filter out arrays of size one or less.
+        // We do this so we won't face out of bounds error when calculate prefixes
         if (arr.length <= 1) {
             return;
         }
 
 
         int maxLen = 0;
+
+        // Final array where inputs will be sorted.
         byte[][] finalArr = new byte[arr.length][];
 
+
+        // Find the max length of a subarray.
         for (byte[] subArr : arr) {
+
+            // If the current subarray is greater than max, update max with current subarray length
             if (maxLen < subArr.length) {
                 maxLen = subArr.length;
             }
         }
 
+
+        // Count will be the largest sub array length plus one
+        // We will do this because we know that all lengths will be between 0 and the max length
         int[] count = new int[maxLen+1];
+
+        // This will be the same length as count
         int[] prefix = new int[maxLen+1];
 
 
+        // loop through each subarray in the array. Update the count in the count array
+        // For example, the subarrays with length two are in count[2]
         for (byte[] subArr : arr) {
             count[subArr.length]++;
         }
 
 
-
+        // Make both first elements equal to each other to make algorithm work
         prefix[0] = count[0];
         for (int i = 1; i <= maxLen; i++) {
+            // We make the prefix equal to the number of occurrences plus the previous prefix sum
             prefix[i] = prefix[i-1] + count[i];
         }
 
 
+
+        // We loop backwards since we want to maintain stability
         for (int i = arr.length-1; i >= 0; i--) {
+            // We get the length of the subarray
             int len = arr[i].length;  // 4
+
+            // This will be where we place the subarray in the sorted array
             int idx = prefix[len] - 1;
+
+            // Place subarray in final array
             finalArr[idx] = arr[i];
+
+            // Subtract 1 from prefix length
+            // This ensures that the next array of the same length goes in the right index
             prefix[len]--;
         }
 
 
+
+        // Copy the items of the finalArr into the initial array for test cases
         System.arraycopy(finalArr, 0, arr, 0, arr.length);
 
 
